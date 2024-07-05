dbafa5bdaa66bc22e439317a7b3eee3f470311d9
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-05 12:34:41 +0530

    first commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+         <div className="textstructure mt-40 px-20">
+          <div className="masker ">
+                    <h1 className="uppercase text-9xl -leading-[2vw] tracking-tighter font-['Poppins'] font-medium">We Create</h1>
+          </div>
+          <div className="masker ">
+                    <h1 className="uppercase text-9xl leading-[4vw] tracking-tighter font-['Poppins'] font-medium">Eye Opening</h1>
+          </div>
+          <div className="masker ">
+                    <h1 className="uppercase text-9xl leading-[4vw] tracking-tighter font-['Poppins'] font-medium">Presentations</h1>
+          </div>
+          </div> 
+    </div>
+  )
+}
+
+export default LandingPage
